330d12930ae27bf231753206441da859ba137862
feng.zhai <feng.zhai@chinacache.com>
2018-04-26 01:23:01 +0800

    Feature: Add `TestTreeNode2Slice` func

--- tools/tree.go
+++ tools/tree.go
@@ -1,10 +1,14 @@
 package tools
 
 import (
+	"fmt"
+
 	"github.com/zhai3516/leetcode-tools/model"
+
+	"github.com/golang-collections/go-datastructures/queue"
 )
 
-func IntSlice2TreeNodes(s []interface{}) *model.TreeNode {
+func Slice2TreeNodes(s []interface{}) *model.TreeNode {
 	if s == nil || len(s) == 0 {
 		return nil
 	}
@@ -42,4 +46,48 @@ func IntSlice2TreeNodes(s []interface{}) *model.TreeNode {
 	}
 
 	return treeLists[0]
+}
+
+func TreeNode2Slice(root *model.TreeNode) []interface{} {
+	var results []interface{} = make([]interface{}, 0)
+
+	if root == nil {
+		return results
+	}
+
+	var rootFlag = true
+	var que = queue.New(0)
+	que.Put(root)
+
+	// do while is better
+	for !que.Empty() {
+		tempTree, _ := que.Get(1)
+		if tempTree[0] != nil {
+			results = append(results, tempTree[0].(*model.TreeNode).Val)
+
+			if tempTree[0].(*model.TreeNode).Left != nil {
+				que.Put(tempTree[0].(*model.TreeNode).Left)
+			} else if rootFlag || que.Len() > 0 {
+				// root should be trade differe
+				que.Put(nil)
+				rootFlag = false
+			}
+
+			if tempTree[0].(*model.TreeNode).Right != nil {
+				que.Put(tempTree[0].(*model.TreeNode).Right)
+			} else if que.Len() > 1 {
+				que.Put(nil)
+			}
+		} else {
+			results = append(results, nil)
+		}
+		rootFlag = false
+	}
+
+	if results[len(results)-1] == nil {
+		results = append(results[:len(results)-1])
+	}
+
+	fmt.Println(results)
+	return results
 }
--- tools/tree_test.go
+++ tools/tree_test.go
@@ -1,27 +1,58 @@
 package tools
 
-import "testing"
+import (
+	"testing"
 
-func TestIntSlice2TreeNode(t *testing.T) {
-	if IntSlice2TreeNodes(nil) != nil {
-		t.Error("Error with: nil")
+	"github.com/zhai3516/leetcode-tools/model"
+)
+
+func TestSlice2TreeNode(t *testing.T) {
+	if Slice2TreeNodes(nil) != nil {
+		t.Error("Slice2TreeNode error with: nil")
 	}
 
 	var empty []interface{}
-	if IntSlice2TreeNodes(empty) != nil {
-		t.Error("Error with: []")
+	if Slice2TreeNodes(empty) != nil {
+		t.Error("Slice2TreeNode error with: []")
 	}
 
 	var tree1 = []interface{}{1, 2, 3}
-	root := IntSlice2TreeNodes(tree1)
+	root := Slice2TreeNodes(tree1)
 	if root.Val != 1 || root.Left.Val != 2 || root.Right.Val != 3 {
-		t.Error("Error with: [1, 2, 3]")
+		t.Error("Slice2TreeNode error with: [1, 2, 3]")
 	}
 
 	var tree2 = []interface{}{1, nil, 2, 3}
-	root = IntSlice2TreeNodes(tree2)
+	root = Slice2TreeNodes(tree2)
 	if root.Val != 1 || root.Left != nil || root.Right.Val != 2 || root.Right.Left.Val != 3 {
-		t.Error("Error with: [1, nil, 2, 3]")
+		t.Error("Slice2TreeNode error with: [1, nil, 2, 3]")
 	}
 
+}
+
+func TestTreeNode2Slice(t *testing.T) {
+	var tree = &model.TreeNode{Val: 0}
+	var treeList, results []interface{}
+
+	if len(TreeNode2Slice(nil)) != 0 {
+		t.Error("TreeNode2Slice error with: nil")
+	}
+
+	tree.Val = 1
+	if len(TreeNode2Slice(tree)) != 1 {
+		t.Error("TreeNode2Slice error with: [1]")
+	}
+
+	treeList = []interface{}{1, nil, 2, 3}
+	tree = Slice2TreeNodes(treeList)
+	results = TreeNode2Slice(tree)
+	if len(results) != len(treeList) {
+		t.Error("TreeNode2Slice error with: [1, nil, 2, 3]")
+	}
+	for i, v := range treeList {
+		if v != results[i] {
+			t.Error("TreeNode2Slice error with: [1, nil, 2, 3]")
+			break
+		}
+	}
 }
